06ea70ee6a2f8e6f23c80423cfdaca25654a124a
Reydifox <reydifox@gmail.com>
2021-05-08 17:52:35 +0200

    minpoly
    minimal polynomial estimation test

--- RAL_Assignment/src/Main.cpp
+++ RAL_Assignment/src/Main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <NTL/ZZ.h>
 #include <NTL/ZZ_p.h>
 #include <NTL/ZZ_pX.h>
@@ -8,6 +9,22 @@
 
 NTL_CLIENT
 
+void getMinPoly(StringArray &seq, std::vector<ZZ_pX> &out, int len, int field)
+{
+	for (int s = 0; s < seq.size(); s++) 
+	{
+		vec_ZZ_p vec;
+		vec.SetLength(len);
+
+		for (int i = 0; i < len; i++)
+		{
+			vec.put(i, conv<ZZ_p>(seq.at(s).at(i)));
+		}
+		ZZ_pX poly;
+		MinPolySeq(poly, vec, field);
+		out.push_back(poly);
+	}
+}
 
 int main()
 {
@@ -21,7 +38,7 @@ int main()
 
 	// Generate and save sequences n mod p
 	std::string buf;
-	Permutation::generateStringSequence(length, field, buf, true);
+	Permutation::generateStringSequence(length, field, buf, false);
 
 	// Parse buffer to the string array
 	StringArray arr;
@@ -30,6 +47,14 @@ int main()
 	ZZ_p::init(conv<ZZ>(field));
 
 	// TODO compute minimal polynomial from given sequence
+	std::vector<ZZ_pX> polynomials;
+	getMinPoly(arr, polynomials, length, field);
+
+	for (const auto& p : polynomials)
+	{
+		cout << p << endl;
+	}
+
 	// TODO get coeff from polynomial
 	// TODO generate CSV
 
